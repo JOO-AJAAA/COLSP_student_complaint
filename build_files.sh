d6eafde8b0e529ff75480e7eaf6ba8dc2b4e663b
yohanes <mrxg11683@gmail.com>
2025-12-01 19:14:12 +0700

    Fix build script permissions and LF

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,4 @@
-pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
+echo "Building project..."
+python3 -m pip install -r requirements.txt
+python3 manage.py collectstatic --noinput --clear
+echo "Build End"
